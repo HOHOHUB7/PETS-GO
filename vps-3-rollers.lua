717eda67e420f88420585fb749d2a9a74c49c431
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-03 23:13:57 -0300

    Update vps-3-rollers.lua

--- vps-3-rollers.lua
+++ vps-3-rollers.lua
@@ -1,3 +1,12 @@
+-- SCRIPT KEY
 script_key = "dPPhgrEyLHxstNrHqMfFEhPPrbxFYrPE";
 
+-- YUMMY CONFIG
+_G.Config = { 
+    UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", 
+    discord_id = "1037147029614362624", 
+    Note = "VPS - 3", 
+} 
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HOHOHUB7/PETS-GO/refs/heads/main/script-alt.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/petsgo"))()
