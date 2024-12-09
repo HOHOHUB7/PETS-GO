0360ee19de21a45ba86157a0844f1271d17c7dc5
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-09 19:54:24 -0300

    Update ug-phone-seller.lua

--- ug-phone-seller.lua
+++ ug-phone-seller.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/074ae419ea26f2a773d8e592958ec2e8.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HOHOHUB7/PETS-GO/refs/heads/main/script-auto-seller.lua"))() 
