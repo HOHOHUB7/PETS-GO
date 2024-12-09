d6064f0ac26d63d3095b1248ff2c185040327b39
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-09 19:57:24 -0300

    Update script-auto-seller.lua

--- script-auto-seller.lua
+++ script-auto-seller.lua
@@ -52,4 +52,5 @@ getgenv().petsGoConfig = {
     MAIL_GEMS_MIN = 1000000,  
 }
 
+game:GetService("RunService"):Set3dRenderingEnabled(false)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/074ae419ea26f2a773d8e592958ec2e8.lua"))() 
