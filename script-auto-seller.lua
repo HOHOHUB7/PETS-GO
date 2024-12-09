41f9b701de98a5fd6e84948cc0b050a8ce461221
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-09 19:53:01 -0300

    Update script-auto-seller.lua

--- script-auto-seller.lua
+++ script-auto-seller.lua
@@ -1,1 +1,58 @@
 
+script_key="dOfSrLWDyauFbCSkkEFAHvWlUvslwqrV";
+
+
+getgenv().petsGoConfig = {
+    DISCORD_ID = "1037147029614362624",
+    PLAZA_WEBHOOK_URL = "https://discord.com/api/webhooks/1305321371978436608/WA_DConhxKPY4ELAB-jWllN3XEsbQnCL9rnGsltO9eueOFzYz4vSubFLTS8Df918dTze",
+    MAIL_GEM_WEBHOOK_URL = "https://discord.com/api/webhooks/1310890061037961277/aoU8MFehs2VoU3I2JgMDCqNdhSeV9Bhzt0attsXmffAM_IDtZuyISFnKCBp53Z1n-U1s",
+
+    PLAZA_HOP_DELAY = 15,  
+
+    SELL_10M_PET = false,  
+    SELL_10M_PET_MIN = 10, 
+    SELL_PERCENTAGE_10M = 1,
+
+    SELL_EGG = true,
+    SELL_EGG_MIN = 5,  
+    SELL_EGG_PERCENTAGE = 1,   -
+
+    SELL_25M_PET = false, 
+    SELL_PERCENTAGE_25M = 1,  
+
+    SELL_50M_PET = false, 
+    SELL_PERCENTAGE_50M = 1, 
+
+    SELL_100M_PET = true, 
+    SELL_PERCENTAGE_100M = 1,  
+
+    SELL_HUGE = true,  
+    SELL_SECRET = true,  
+    SELL_PERCENTAGE_HUGE = 1,  
+    SELL_PERCENTAGE_SECRET = 1, 
+
+    SELL_POTION = true,  
+    SELL_POTION_MIN = 5, 
+    SELL_POTION_PERCENTAGE = 1.2,  
+
+    SELL_GOD_POTION = true, 
+    SELL_GOD_POTION_PERCENTAGE = 1, 
+
+    SELL_KEY = true,  
+    SELL_KEY_MIN = 10,
+    SELL_KEY_PERCENTAGE = 1, 
+    SELL_KEY_LOWER = true,  
+    SELL_KEY_UPPER = false,  
+
+    SELL_BAIT = true,  
+    SELL_BAIT_MIN = 3,  
+    SELL_BAIT_PERCENTAGE = 1,  
+
+    SELL_CHEST = true,  
+    SELL_CHEST_MIN = 3, 
+    SELL_CHEST_PERCENTAGE = 1,  
+
+    MAIL_GEMS_USERNAME = "FASTZIN_STORE", 
+    MAIL_GEMS_MIN = 1000000,  
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/074ae419ea26f2a773d8e592958ec2e8.lua"))() 
