5b5dd9e3cc5202fa274e8391f8e142d7dc6b83fc
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-11 07:41:22 -0300

    Create adopt-me-kaitun.lua

---
+++ adopt-me-kaitun.lua
@@ -0,0 +1,10 @@
+script_key="YGrKhPfsuQRouMckrGHzIUguqWoYkcQH";
+getgenv().Config = {
+    ["Winter_EVENT"] = false,
+    ["PetFarmActive"] = true,
+    ["EggFarmActive"] = false,
+    ["Blur_username"] = true,
+    ["Blazing_Lion_Log"] = false,
+    ["DiscordId"] = "123456",
+    ["Webhook"] = "https://discord.com/api/webhooks/123456",
+}loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
