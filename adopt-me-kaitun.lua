d14e2d4b47564e3bca429173c33b9b92fab06dbe
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-12 08:36:50 -0300

    Update adopt-me-kaitun.lua

--- adopt-me-kaitun.lua
+++ adopt-me-kaitun.lua
@@ -7,4 +7,13 @@ getgenv().Config = {
     ["Blazing_Lion_Log"] = false,
     ["DiscordId"] = "123456",
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
-}loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+}
+
+_G.Config = { 
+    UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", 
+    discord_id = "1037147029614362624" , 
+        Note = "VPS", 
+} 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/adoptmee"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
