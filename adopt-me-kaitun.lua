ea89dd7c84a24b583bff2495bd44df3092d9eef4
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-18 16:37:28 -0300

    Update adopt-me-kaitun.lua

--- adopt-me-kaitun.lua
+++ adopt-me-kaitun.lua
@@ -9,16 +9,10 @@ getgenv().Config = {
     ["Webhook"] = "https://discord.com/api/webhooks/123456",
 }
 
-_G.Config = { 
-    UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", 
-    discord_id = "1037147029614362624" , 
-        Note = "VPS", 
-} 
-
 task.spawn(function()
-    pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/adoptmee"))()
-    end)
+    task.wait(35)
+    getfenv().url = "http://xdiw.xyz/update/felps4_20?api_key=252b9c71516448b6832fe5fe1cc1386f"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/darzex/dashboard/main/main.lua"))()
 end)
 
 task.spawn(function()
